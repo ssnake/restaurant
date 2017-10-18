5c10681eaa28571e2acfa83a957657316097de71
Yar-ua <yar.ua.1985@gmail.com>
2017-10-19 02:05:56 +0300

    some refactoring

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -1,6 +1,6 @@
 class ProductType < ApplicationRecord
   # создание связи один ко многим - имеет много :products
-  has_many :products
+  has_many :products, dependent: :destroy
 
   # название категории меню должно быть, и не длиннее 50 знаков
   validates :title, presence: true, length: {maximum: 50}
