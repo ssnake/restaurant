6005d08599611c8f76c7738eb22f30dbd75b7dee
Yar-ua <yar.ua.1985@gmail.com>
2017-10-20 10:36:45 +0300

    refactore routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   
-  #get 'product_types/index'
-
-  #get 'product_types/new'
-
-  #get 'product_types/show'
+  # общая точка входа
+  get '/' => 'product_types#index'
 
   # в ресурсе :product_types вводим вложенный ресурс :products
   resources :product_types do
