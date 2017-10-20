d9002b93c5e34bb12b2d3ccd7ac345d6fc54ba0e
Yar-ua <yar.ua.1985@gmail.com>
2017-10-20 21:57:53 +0300

    add delede product method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product_type, only: [:new, :create, :show, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   
   def index
@@ -8,7 +9,7 @@ class ProductsController < ApplicationController
 
   
   def new
-
+    @product = @product_type.products.build
   end
 
 
@@ -23,42 +24,46 @@ class ProductsController < ApplicationController
 
 
   def show
-    @product = @product_type.products.find(params[:id])
+
   end
 
 
   def edit
-    @product = @product_type.products.find(params[:id])
+
   end
 
 
   def update
-    @product = @product_type.products.find(params[:id])    
+   
     if @product.update(product_params)
-      redirect_to product_types_path, method: :patch, notice: 'Блюдо обновлено в меню'
+      redirect_to product_type_path(id: @product.product_type_id), method: :patch, notice: 'Блюдо обновлено в меню'
     else
       render 'edit', alert: 'Что то пошло не так'
     end
   end
 
-  # DELETE /products/1
-  # DELETE /products/1.json
+
   def destroy
     @product.destroy
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to product_type_path(id: @product.product_type_id), notice: 'Блюдо удалено из меню'
   end
 
+
   private
-    # Use callbacks to share common setup or constraints between actions.
+  
+
     def set_product_type
       @product_type = ProductType.find(params[:product_type_id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
+    def set_product
+      @product = @product_type.products.find(params[:id])    
+    end
+
+
     def product_params
       params.require(:product).permit(:title, :description, :price)
     end
+
 end
