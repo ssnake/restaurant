935eaa39979ea373ee743397e633b7be9377de16
Yar-ua <yar.ua.1985@gmail.com>
2017-10-21 13:38:34 +0300

    refactoring style

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,9 @@ class Product < ApplicationRecord
   # создание связи один ко многим - следовать за product_type
   belongs_to :product_type
 
+  # определяем методы валидации продукта
   validates :title, presence: { message: 'Название не может быть пустым' }
   validates :title, length: { maximum: 30, message: 'Название не может быть длиннее 30 символов' }
   validates :description, length: { maximum: 255, message: 'Описание блюда должно быть не длиннее 255 символов' }
-  validates :price, numericality: { message: 'Значение стоимости должно быть числом' }
-#  validates :price, greater_than: { 0, message: 'Значение цены должно быть больше 0' }
+  validates :price, numericality: { greater_than: 0, message: 'Значение стоимости должно быть числом, и больше ноля' }
 end
