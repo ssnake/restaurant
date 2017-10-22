735c6bf88c0eb3501a569d7ca9b5cad1dc7b7715
Yar-ua <yar.ua.1985@gmail.com>
2017-10-22 15:03:39 +0300

    ready to changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   
-  resources :line_items
-  resources :carts
   devise_for :users
   # общая точка входа
   root to: 'product_types#index'
@@ -11,5 +9,9 @@ Rails.application.routes.draw do
   resources :product_types do
   	resources :products
   end
+
+  resources :line_items
+  resources :carts
+
   
 end
