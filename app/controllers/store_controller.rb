6825917f54f8413d1d0a5b7c81cddd0cb8d34cdf
Yar-ua <yar.ua.1985@gmail.com>
2017-10-23 16:10:36 +0300

    add new  controller Store index show

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,12 @@
+class StoreController < ApplicationController
+  def index
+  	@product_types = ProductType.all
+  	@products_all = Product.order(:title)
+  end
+
+  def show
+    @product_type = ProductType.find(params[:id])
+    @product_types = ProductType.all
+    @products_by_product_type = @product_type.products.order('title ASC')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   
+  root to: 'store#index'
+
+  resources :store, only: [:index, :show]
+  get 'store/show'
+
   devise_for :users
   # общая точка входа
-  root to: 'product_types#index'
+  #root to: 'product_types#index'
   get '/' => 'product_types#index'
 
   # в ресурсе :product_types вводим вложенный ресурс :products
