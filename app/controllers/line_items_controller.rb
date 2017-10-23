5f8fef82c9f5c7507c82db16d986bf8d053dbdcd
Yar-ua <yar.ua.1985@gmail.com>
2017-10-23 16:56:21 +0300

    add update quantity of line items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,7 +2,7 @@ class LineItemsController < ApplicationController
   # подключаем созданный модуль по обнаружению корзины
   include CurrentCart
   # установить корзину перед записью туда блюда
-  before_action :set_cart, only: [:create, :edit]
+  before_action :set_cart, only: [:index, :create, :edit]
 
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
@@ -47,8 +47,8 @@ class LineItemsController < ApplicationController
   # PATCH/PUT /line_items/1.json
   def update
     respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
+      if @line_item.update(line_item_quantity_params)
+        format.html { redirect_to @line_item.cart, notice: 'Количество обновлено' }
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit }
@@ -80,5 +80,9 @@ class LineItemsController < ApplicationController
 
     def line_item_params
       params.require(:line_item).permit(:product_id)
+    end
+
+    def line_item_quantity_params
+      params.require(:line_item).permit(:quantity)
     end
 end
