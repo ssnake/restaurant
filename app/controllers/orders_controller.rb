691f3c3d817fdbfa04e8c7207bcbf3a18da98c15
Yar-ua <yar.ua.1985@gmail.com>
2017-11-01 00:05:11 +0200

    view single orders - added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
+    @orders = Order.where(name: current_user.name)
   end
 
   # GET /orders/1
