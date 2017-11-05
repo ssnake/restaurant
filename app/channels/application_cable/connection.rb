0006548e16cfb14ddba33ff901950dc48c045eca
Yar-ua <yar.ua.1985@gmail.com>
2017-11-05 16:26:11 +0200

    smth 1

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+  	
+  	identified_by :current_user
+
+  	def conncet
+  	  self.current_user = find_verifieed_user
+  	end
+
+
+  	private
+
+  	def find_verified_user
+  	  if verified_user = User.find_by(id: cookies.encrypted[:user_id])
+  	  	verified_user
+  	  else
+  	  	reject_unautorized_connection
+  	  end
+  	end
+
   end
 end
---
+++ app/channels/line_channel.rb
@@ -0,0 +1,8 @@
+class LineChannel < ApplicationCable::Channel
+
+  # Вызывается, когда потребитель успешно
+  # стал подписчиком этого канала
+  def subscribed
+  end
+  
+end
